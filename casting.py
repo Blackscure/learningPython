5cafa1b3b5a8f4a4c9d0f4e673fcfa0866a771dc
blackscure <weksabuyahi@gmail.com>
2025-08-24 11:11:31 +0300

    casting

---
+++ casting.py
@@ -0,0 +1,13 @@
+# We use data type when we want to specify data type of a variable
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
