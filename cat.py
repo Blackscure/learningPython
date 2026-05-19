6241e503c5381bcb725c712cff01331669aed024
Blackscure <wekesabuyahi@gmail.com>
2026-05-19 11:21:50 +0300

    dict added

--- cat.py
+++ cat.py
@@ -1,5 +1,7 @@
-i = 1
+# i = 1
 
-while i <= 3:
-    print("Meow")
-    i += 1
+# while i <= 3:
+#     print("Meow")
+#     i += 1
+
+print("meow\n" * 3, end="")
---
+++ hogwarts.py
@@ -0,0 +1,9 @@
+students = [
+    {"name":"Hezzy", "house":"Jordwam aparments", "location":"Fedha"},
+    {"name":"Essy", "house":"Bgm aparments", "location":"Bungoma"},
+    {"name":"Fina", "house":"Jordan aparments", "location":"Fedha"},
+]
+
+
+for student in students:
+    print(student["name"], student["house"], student["location"], sep=",  ")
